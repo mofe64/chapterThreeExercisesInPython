49c21a648793e3f04c6bb3b219bb80b67932f9ee
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-01-17 23:30:00 +0100

    account class and tests

--- account.py
+++ account.py
@@ -1,5 +1,29 @@
 class Account:
 
     def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
+        self._firstname = firstname
+        self._lastname = lastname
+        self._balance = 0
+
+    def get_firstname(self):
+        return self._firstname
+
+    def get_lastname(self):
+        return self._lastname
+
+    def set_lastname(self, lastname):
+        self._lastname = lastname
+
+    def set_firstname(self, firstname):
+        self._firstname = firstname
+
+    def get_balance(self):
+        return self._balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+
+    def withdraw(self, amount):
+        if amount < self._balance:
+            self._balance -= amount
--- test_account.py
+++ test_account.py
@@ -5,10 +5,29 @@ from account import Account
 class TestAccount(TestCase):
     def setUp(self) -> None:
         self.my_account = Account("Stan", "Lee")
-        print("testing")
+        print("setting up")
 
     def tearDown(self) -> None:
-        print("torn")
+        self.my_account = None
+        print("tearing down")
 
     def test_account_is_initialized_with_passed_in_values(self):
-        print("test")
+        self.assertEqual("Stan", self.my_account.get_firstname())
+        self.assertEqual("Lee", self.my_account.get_lastname())
+
+    def test_account_name_can_be_set(self):
+        self.my_account.set_firstname("Tim")
+        self.my_account.set_lastname("Cook")
+        self.assertEqual("Tim", self.my_account.get_firstname())
+        self.assertEqual("Cook", self.my_account.get_lastname())
+
+    def test_user_can_withdraw_and_deposit(self):
+        self.my_account.deposit(10000)
+        self.assertEqual(10000, self.my_account.get_balance())
+        self.my_account.withdraw(5000)
+        self.assertEqual(5000, self.my_account.get_balance())
+
+    def test_user_cannot_withdraw_more_than_amount_in_account(self):
+        self.my_account.deposit(100)
+        self.my_account.withdraw(50000)
+        self.assertEqual(100, self.my_account.get_balance())
